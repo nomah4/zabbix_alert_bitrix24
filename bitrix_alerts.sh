f7f3c699a544071a911ea549728ce3afccb9c933
Mikhail Gorbachev <31391617+nomah4@users.noreply.github.com>
2024-06-22 08:46:36 +0300

    modify script to any chats

--- bitrix_alerts.sh
+++ bitrix_alerts.sh
@@ -1,22 +1,19 @@
   GNU nano 5.4                                                                    bitrix_alerts.sh
 #!/bin/bash
-
 # Parameters
-to=chatXXXXX //change it to ID of channel you want to send to
-subject="$1"
+to=$1 
 body="$2"
 now=$(date)
 
 # Bitrix24 URL webhook
-webhook_url="https://xxxx.bitrix24.pl/rest/85/xxxx/im.message.add.json" // here change it to your bitrix24 webhook
+webhook_url="https://xxxx.bitrix24.pl/rest/85/xxxx/im.message.add.json" #  here change it to your bitrix24 webhook
 
-# Massage formating
+# Message formating
 message="${body}"
 
 # Logging whats send
 echo "[Zabbix Problem]" + "$now" >> /usr/lib/zabbix/alertscripts/problem.txt
 echo "$message" >> /usr/lib/zabbix/alertscripts/problem.txt
-
 # Sending message to Bitrix24 and logging it in response.txt
 echo "" >> /usr/lib/zabbix/alertscripts/response.txt
 echo "" >> /usr/lib/zabbix/alertscripts/response.txt
